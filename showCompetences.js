01f2c2e0ee732e48546cea33ab66c8dea6a2f0f4
lmorganti <leonardo.morganti00@gmail.com>
2020-11-06 16:13:21 +0100

    Update issue 2: requested changes
    I cambiamenti richiesti per la issue 2 sono stati risolti

--- showCompetences.js
+++ showCompetences.js
@@ -1,9 +1,3 @@
-// let users = [
-//     { name: "Gabriele Puliti", competencesSO: { ubuntu: 4, windows: 2 }, competencesProgramming: { java: 3, cPlusPlus: 3 } },
-//     { name: "Leonardo Scrivere", competencesSO: { linux: 2, macOS: 1 }, competencesProgramming: { ruby: 2, javascrip: 4 } },
-//     { name: "Pierino Marameo", competencesSO: { Solaris: 3, windows: 2 }, competencesProgramming: { java: 1, cSharp: 2 } },
-// ];
-
 let users = [
     {
         userName: "Mario Rossi",
@@ -37,36 +31,26 @@ let users = [
     }
 ];
 
-
-// let spaceShowUsers= document.getElementById("show");
-// clear(spaceShowUsers);
-
-    for (let index = 0; index < users.length; index++) {
-
-
-
-        document.getElementById("show").innerHTML += "<ul><br><li> Nome e Cognome: " + users[index].userName + "</li><br> <li> Competenze: <br> " + printArrayOfObject(users[index].competenceUser) + "</ul><br><br><hr>";
-
-
-    }
-
-
+for (let index = 0; index < users.length; index++) {
+    document.getElementById("show").innerHTML += "<ul>\
+    <br><li> Nome e Cognome: " + users[index].userName + "</li>\
+    <br><li> Competenze: \
+        <br>" + printArrayOfObject(users[index].competenceUser) +
+        "</ul><br><br><hr>";
+}
 
 function clear(space) {
-
     space.innerHTML="";
-
 }
 
 function printArrayOfObject(arrOfobj) {
     var output = "";
-
-
     for (let index = 0; index < arrOfobj.length; index++) {
-        output += "<br>  <ul><li>Competenza Conosciuta: "+arrOfobj[index].nameComp + "</li><li>   AreaCompetenza: "+arrOfobj[index].area+ "</li><li> Livello Competenza:"+arrOfobj[index].level+ "</ul><br><br>";
-        
+        output += "<br><ul>\
+        <li>Competenza Conosciuta: "+arrOfobj[index].nameComp + "</li>\
+        <li>AreaCompetenza: "+arrOfobj[index].area+ "</li>\
+        <li>Livello Competenza:"+arrOfobj[index].level+ "</li>\
+        </ul><br><br>";   
     }
-
     return output;
-
 }
