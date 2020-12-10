7da95332b9f54d55559a393bd46e908a7c39f4a7
mate94 <73949030+mate94@users.noreply.github.com>
2020-12-10 16:07:39 +0100

    Closes #16
    Progetto Spring per gestione competenze

---
+++ src/main/java/plansoft/gestionecompetenze/GestionecompetenzeApplication.java
@@ -0,0 +1,13 @@
+package plansoft.gestionecompetenze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionecompetenzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionecompetenzeApplication.class, args);
+	}
+
+}
