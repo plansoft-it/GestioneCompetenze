07f91325d38a856314fb0ce2f478ea7797f2313f
mate94 <73949030+mate94@users.noreply.github.com>
2020-12-15 15:24:34 +0100

    Formattazione entità Dipendente: nuova modifica ai metodi get e set della classe Employee

--- src/main/java/plansoft/skillsmanagement/employee/Employee.java
+++ src/main/java/plansoft/skillsmanagement/employee/Employee.java
@@ -30,22 +30,23 @@ public class Employee {
 	public void setId(String id) {
 		this.id = id;
 	}
-	
-	public String getNome() {
+
+	public String getName() {
 		return name;
 	}
-	
-	public void setNome(String name) {
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public String getCognome() {
+
+	public String getSurname() {
 		return surname;
 	}
-	
-	public void setCognome(String surname) {
+
+	public void setSurname(String surname) {
 		this.surname = surname;
 	}
 	
+
 	
 }
