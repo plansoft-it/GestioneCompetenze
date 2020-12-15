a85fa137f0a47873997da0a4dd4459f369225b3b
mate94 <73949030+mate94@users.noreply.github.com>
2020-12-15 12:15:13 +0100

    Formattazione entità Dipendente: modifica dei nomi dei parametri di ingresso nella classe Dipendente (da italiano a inglese)

--- src/main/java/plansoft/skillsmanagement/employee/Employee.java
+++ src/main/java/plansoft/skillsmanagement/employee/Employee.java
@@ -16,26 +16,33 @@ public class Employee {
 	public Employee() {
 
 	}
+	
 	public Employee(String id, String name, String surname) {
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
 	}
+	
 	public String getId() {
 		return id;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
+	
 	public String getNome() {
 		return name;
 	}
+	
 	public void setNome(String name) {
 		this.name = name;
 	}
+	
 	public String getCognome() {
 		return surname;
 	}
+	
 	public void setCognome(String surname) {
 		this.surname = surname;
 	}
