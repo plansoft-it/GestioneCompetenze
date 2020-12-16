b0d748a770ecf3a6e840a534bcee6cfe617b835a
mate94 <73949030+mate94@users.noreply.github.com>
2020-12-16 16:49:53 +0100

    Cartella dipendente eliminata

--- src/main/java/plansoft/skillsmanagement/dipendente/DipendenteRepository.java
+++
@@ -1,9 +0,0 @@
-package plansoft.gestionecompetenze.dipendente;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DipendenteRepository extends CrudRepository<Dipendente, String>{
-	
-	
-	
-}
