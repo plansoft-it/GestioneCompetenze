a3e10115ff8478eda7519341acf9646a3d281313
scrivereleonardo <leoscrivere@hotmail.it>
2021-01-07 11:42:59 +0100

    Ultime Modifiche

--- Angular/GestioneCompetenze/src/app/dipendenti-inteface.ts
+++ Angular/GestioneCompetenze/src/app/dipendenti-inteface.ts
@@ -1,5 +1,6 @@
 export interface Dipendente {
     userName: string;
+    userSurname: string;
     id?:number;
     competenceUser:{nameComp: string, area: string, level: number} []    //l ho messo opzionale perchè magari ancora non ha comptenze    
 }                                                                          //ricordati sempre il tipo
